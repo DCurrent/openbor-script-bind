e2bba3a563c5129fb5c2308d344a12ad88db69df
Caskey, Damon V <dc@caskeys.com>
2018-11-19 09:29:35 -0500

    Instance functions.

--- config.c
+++ config.c
@@ -1,1 +1,19 @@
 #include "data/scripts/dc_bind/config.h"
+
+// Which instance is in use?
+int dc_bind_get_instance()
+{
+	void result = getlocalvar(DC_BIND_VAR_KEY_INSTANCE);
+
+	if (!result)
+	{
+		result = DC_BIND_DEFAULT_INSTANCE;
+	}
+
+	return result;
+}
+
+void dc_bind_set_instance(int value)
+{
+	setlocalvar(DC_BIND_VAR_KEY_INSTANCE, value);
+}
