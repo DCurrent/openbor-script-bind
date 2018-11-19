f46849b2715efbf2b378ef398559b9d79f9ae843
Caskey, Damon V <dc@caskeys.com>
2018-11-19 17:00:13 -0500

    Swap default ent and targets.

--- config.h
+++ config.h
@@ -12,14 +12,14 @@
 
 // Default values.
 #define DC_BIND_DEFAULT_INSTANCE	0
-#define DC_BIND_DEFAULT_ENT			getlocalvar("self")
+#define DC_BIND_DEFAULT_ENT			DC_BIND_TARGET_SCREEN
 #define DC_BIND_DEFAULT_ENABLE_X	DC_BIND_ENABLED
 #define DC_BIND_DEFAULT_ENABLE_Y	DC_BIND_ENABLED
 #define DC_BIND_DEFAULT_ENABLE_Z	DC_BIND_ENABLED
 #define DC_BIND_DEFAULT_OFFSET_X	0
 #define DC_BIND_DEFAULT_OFFSET_Y	0
 #define DC_BIND_DEFAULT_OFFSET_Z	0
-#define DC_BIND_DEFAULT_TARGET		DC_BIND_TARGET_SCREEN
+#define DC_BIND_DEFAULT_TARGET		getlocalvar("self")
 #define DC_BIND_DEFAULT_SCREEN_X	dc_bind_find_screen_center_x()
 #define DC_BIND_DEFAULT_SCREEN_Y	dc_bind_find_screen_center_y()
 #define DC_BIND_DEFAULT_SCREEN_Z	openborconstant("SCREENPANEL_Z")
