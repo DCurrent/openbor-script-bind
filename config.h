6126ff0bb8beda131f761e5347cf2a62d7947567
Caskey, Damon V <dc@caskeys.com>
2018-11-19 09:13:03 -0500

    Add config header.

---
+++ config.h
@@ -0,0 +1,20 @@
+#ifndef DC_BIND_CONFIG
+#define DC_BIND_CONFIG 1
+
+// Options.
+#define	DC_BIND_OFFSET_DISABLE		NULL()	
+
+// Default values.
+#define DC_BIND_DEFAULT_INSTANCE	0
+#define DC_BIND_DEFAULT_OFFSET_X	0
+#define DC_BIND_DEFAULT_OFFSET_Y	0
+#define DC_BIND_DEFAULT_OFFSET_Z	0
+
+// Variable keys.
+#define DC_BIND_VAR_KEY_INSTANCE	"dcbind_0"
+
+#define DC_BIND_VAR_KEY_OFFSET_X	"dcbind_1"
+#define DC_BIND_VAR_KEY_OFFSET_Y	"dcbind_2"
+#define DC_BIND_VAR_KEY_OFFSET_Z	"dcbind_3"
+
+#endif // !DC_BIND_CONFIG
