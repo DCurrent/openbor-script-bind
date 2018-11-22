4fd03dfd5e3254ee8b07e5f3615299f110ffb962
Caskey, Damon V <dc@caskeys.com>
2018-11-22 12:23:34 -0500

    Var keys now use a concatenated base name and id. Added an end point constant.

--- config.h
+++ config.h
@@ -27,16 +27,19 @@
 #define DC_BIND_DEFAULT_SCREEN_Y		dc_bind_find_screen_center_y()
 #define DC_BIND_DEFAULT_SCREEN_Z		openborconstant("SCREENPANEL_Z")
 
-// Variable keys.
-#define DC_BIND_VAR_KEY_INSTANCE		"dcbind_0"
-#define DC_BIND_VAR_KEY_ENT				"dcbind_1"
-#define DC_BIND_VAR_KEY_POSITIONING_X	"dcbind_2"
-#define DC_BIND_VAR_KEY_POSITIONING_Y	"dcbind_3"
-#define DC_BIND_VAR_KEY_POSITIONING_Z	"dcbind_4"
-#define DC_BIND_VAR_KEY_OFFSET_X		"dcbind_5"
-#define DC_BIND_VAR_KEY_OFFSET_Y		"dcbind_6"
-#define DC_BIND_VAR_KEY_OFFSET_Z		"dcbind_7"
-#define DC_BIND_VAR_KEY_TARGET			"dcbind_8"
+// Variable keys. Used to identify cross function
+// variables used throughout library.
+#define DC_BIND_VAR_KEY_BASE_ID			"dcbind"	// Base name of every var ID. Must be unique vs. all other libraries.
+#define DC_BIND_VAR_KEY_INSTANCE		DC_BIND_VAR_KEY_BASE_ID + 0
+#define DC_BIND_VAR_KEY_ENT				DC_BIND_VAR_KEY_BASE_ID + 1
+#define DC_BIND_VAR_KEY_POSITIONING_X	DC_BIND_VAR_KEY_BASE_ID + 2
+#define DC_BIND_VAR_KEY_POSITIONING_Y	DC_BIND_VAR_KEY_BASE_ID + 3
+#define DC_BIND_VAR_KEY_POSITIONING_Z	DC_BIND_VAR_KEY_BASE_ID + 4
+#define DC_BIND_VAR_KEY_OFFSET_X		DC_BIND_VAR_KEY_BASE_ID + 5
+#define DC_BIND_VAR_KEY_OFFSET_Y		DC_BIND_VAR_KEY_BASE_ID + 6
+#define DC_BIND_VAR_KEY_OFFSET_Z		DC_BIND_VAR_KEY_BASE_ID + 7
+#define DC_BIND_VAR_KEY_TARGET			DC_BIND_VAR_KEY_BASE_ID + 8
+#define DC_BIND_VAR_KEY_THE_END			9			// Should always last, and one higher than previous key ID.
 
 // Misc hard code values
 #define DC_BIND_DRAWMETHOD_SCALE_MAX_X	256
